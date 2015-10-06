dd95171bd978247ebaffde0b0edf65ccd3cb26ba
Catherine-Kim <woojong610@gmail.com>
2015-10-06 14:49:46 +0900

    test commit

--- app/src/main/java/com/example/test01/MainActivity.java
+++ app/src/main/java/com/example/test01/MainActivity.java
@@ -4,9 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
-
+//hi
+public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
